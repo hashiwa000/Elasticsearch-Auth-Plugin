32fce7dd484adccdd42d881e74b9f7c85bf0a1ff
hashiwa000 <gate000jp@gmail.com>
2017-10-31 20:15:06 +0900

    simple auth

--- src/jp/hashiwa/elasticsearch/authplugin/AuthPlugin.java
+++ src/jp/hashiwa/elasticsearch/authplugin/AuthPlugin.java
@@ -1,39 +1,19 @@
 package jp.hashiwa.elasticsearch.authplugin;
 
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.bootstrap.BootstrapCheck;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
-import org.elasticsearch.cluster.metadata.IndexTemplateMetaData;
-import org.elasticsearch.cluster.metadata.MetaData;
-import org.elasticsearch.cluster.service.ClusterService;
-import org.elasticsearch.common.component.LifecycleComponent;
-import org.elasticsearch.common.inject.Module;
-import org.elasticsearch.common.io.stream.NamedWriteable;
-import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.logging.Loggers;
-import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.index.IndexModule;
+import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.script.ScriptService;
-import org.elasticsearch.threadpool.ExecutorBuilder;
-import org.elasticsearch.threadpool.ThreadPool;
-import org.elasticsearch.watcher.ResourceWatcherService;
+import org.elasticsearch.rest.*;
 
-import java.io.IOException;
-import java.util.*;
 import java.util.function.UnaryOperator;
 
-/**
- * refer:
- * - https://github.com/codelibs/elasticsearch-auth/blob/master/src/main/java/org/codelibs/elasticsearch/auth/AuthPlugin.java
- * Created by hashiwa on 17/07/08.
- */
-//public class AuthPlugin implements SearchPlugin {
-public class AuthPlugin extends Plugin {
+public class AuthPlugin extends Plugin implements ActionPlugin {
   private Settings settings;
   private final Logger logger = Loggers.getLogger(AuthPlugin.class);
 
@@ -42,157 +22,32 @@ public class AuthPlugin extends Plugin {
     this.logger.info(settings.toString());
   }
 
-  /**
-   * Node level guice modules.
-   */
-  public Collection<Module> createGuiceModules() {
-    return Collections.emptyList();
-  }
-
-  /**
-   * Node level services that will be automatically started/stopped/closed. This classes must be constructed
-   * by injection with guice.
-   */
-//  public Collection<Class<? extends LifecycleComponent>> getGuiceServiceClasses() {
-//    return Collections.emptyList();
+//  @Override
+//  public List<ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
+//    return null;
 //  }
-  public Collection<Class<? extends LifecycleComponent>> getGuiceServiceClasses() {
-    return Arrays.asList(AuthLifecycleComponent.class);
-  }
+//
+//  @Override
+//  public List<Class<? extends ActionFilter>> getActionFilters() {
+//    return null;
+//  }
+//
+//  @Override
+//  public List<RestHandler> getRestHandlers(Settings settings, RestController restController, ClusterSettings clusterSettings, IndexScopedSettings indexScopedSettings, SettingsFilter settingsFilter, IndexNameExpressionResolver indexNameExpressionResolver, Supplier<DiscoveryNodes> nodesInCluster) {
+//    return null;
+//  }
+//
+//  @Override
+//  public Collection<String> getRestHeaders() {
+//    return null;
+//  }
 
-  /**
-   * Returns components added by this plugin.
-   *
-   * Any components returned that implement {@link LifecycleComponent} will have their lifecycle managed.
-   * Note: To aid in the migration away from guice, all objects returned as components will be bound in guice
-   * to themselves.
-   *
-   * @param client AuthLifecycleComponent client to make requests to the system
-   * @param clusterService AuthLifecycleComponent service to allow watching and updating cluster state
-   * @param threadPool AuthLifecycleComponent service to allow retrieving an executor to run an async action
-   * @param resourceWatcherService AuthLifecycleComponent service to watch for changes to node local files
-   * @param scriptService AuthLifecycleComponent service to allow running scripts on the local node
-   */
-  public Collection<Object> createComponents(Client client, ClusterService clusterService, ThreadPool threadPool,
-                                             ResourceWatcherService resourceWatcherService, ScriptService scriptService,
-                                             NamedXContentRegistry xContentRegistry) {
-    return Collections.emptyList();
-  }
-
-  /**
-   * Additional node settings loaded by the plugin. Note that settings that are explicit in the nodes settings can't be
-   * overwritten with the additional settings. These settings added if they don't exist.
-   */
-  public Settings additionalSettings() {
-    return Settings.Builder.EMPTY_SETTINGS;
-  }
-
-  /**
-   * Returns parsers for {@link NamedWriteable} this plugin will use over the transport protocol.
-   * @see NamedWriteableRegistry
-   */
-  public List<NamedWriteableRegistry.Entry> getNamedWriteables() {
-    return Collections.emptyList();
-  }
-
-  /**
-   * Returns parsers for named objects this plugin will parse from {@link XContentParser#namedObject(Class, String, Object)}.
-   * @see NamedWriteableRegistry
-   */
-  public List<NamedXContentRegistry.Entry> getNamedXContent() {
-    return Collections.emptyList();
-  }
-
-  /**
-   * Called before a new index is created on a node. The given module can be used to register index-level
-   * extensions.
-   */
-  public void onIndexModule(IndexModule indexModule) {}
-
-  /**
-   * Returns a list of additional {@link Setting} definitions for this plugin.
-   */
-  public List<Setting<?>> getSettings() { return Collections.emptyList(); }
-
-  /**
-   * Returns a list of additional settings filter for this plugin
-   */
-  public List<String> getSettingsFilter() { return Collections.emptyList(); }
-
-  /**
-   * Provides a function to modify global custom meta data on startup.
-   * <p>
-   * Plugins should return the input custom map via {@link UnaryOperator#identity()} if no upgrade is required.
-   * <p>
-   * The order of custom meta data upgraders calls is undefined and can change between runs so, it is expected that
-   * plugins will modify only data owned by them to avoid conflicts.
-   * <p>
-   * @return Never {@code null}. The same or upgraded {@code MetaData.Custom} map.
-   * @throws IllegalStateException if the node should not start because at least one {@code MetaData.Custom}
-   *                               is unsupported
-   */
-  public UnaryOperator<Map<String, MetaData.Custom>> getCustomMetaDataUpgrader() {
-    return UnaryOperator.identity();
-  }
-
-  /**
-   * Provides a function to modify index template meta data on startup.
-   * <p>
-   * Plugins should return the input template map via {@link UnaryOperator#identity()} if no upgrade is required.
-   * <p>
-   * The order of the template upgrader calls is undefined and can change between runs so, it is expected that
-   * plugins will modify only templates owned by them to avoid conflicts.
-   * <p>
-   * @return Never {@code null}. The same or upgraded {@code IndexTemplateMetaData} map.
-   * @throws IllegalStateException if the node should not start because at least one {@code IndexTemplateMetaData}
-   *                               cannot be upgraded
-   */
-  public UnaryOperator<Map<String, IndexTemplateMetaData>> getIndexTemplateMetaDataUpgrader() {
-    return UnaryOperator.identity();
-  }
-
-  /**
-   * Provides a function to modify index meta data when an index is introduced into the cluster state for the first time.
-   * <p>
-   * Plugins should return the input index metadata via {@link UnaryOperator#identity()} if no upgrade is required.
-   * <p>
-   * The order of the index upgrader calls for the same index is undefined and can change between runs so, it is expected that
-   * plugins will modify only indices owned by them to avoid conflicts.
-   * <p>
-   * @return Never {@code null}. The same or upgraded {@code IndexMetaData}.
-   * @throws IllegalStateException if the node should not start because the index is unsupported
-   */
-  public UnaryOperator<IndexMetaData> getIndexMetaDataUpgrader() {
-    return UnaryOperator.identity();
-  }
-
-  /**
-   * Provides the list of this plugin's custom thread pools, empty if
-   * none.
-   *
-   * @param settings the current settings
-   * @return executors builders for this plugin's custom thread pools
-   */
-  public List<ExecutorBuilder<?>> getExecutorBuilders(Settings settings) {
-    return Collections.emptyList();
-  }
-
-  /**
-   * Returns a list of checks that are enforced when a node starts up once a node has the transport protocol bound to a non-loopback
-   * interface. In this case we assume the node is running in production and all bootstrap checks must pass. This allows plugins
-   * to provide a better out of the box experience by pre-configuring otherwise (in production) mandatory settings or to enforce certain
-   * configurations like OS settings or 3rd party resources.
-   */
-  public List<BootstrapCheck> getBootstrapChecks() { return Collections.emptyList(); }
-
-  /**
-   * Close the resources opened by this plugin.
-   *
-   * @throws IOException if the plugin failed to close its resources
-   */
   @Override
-  public void close() throws IOException {
-
+  public UnaryOperator<RestHandler> getRestHandlerWrapper(ThreadContext threadContext) {
+    return restHandler -> {
+      this.logger.info("in getRestHandlerWrapper: " + restHandler);
+      RestHandler authRestHandler = new AuthRestHandler(restHandler);
+      return authRestHandler;
+    };
   }
 }
-
---
+++ src/jp/hashiwa/elasticsearch/authplugin/AuthRestHandler.java
@@ -0,0 +1,58 @@
+package jp.hashiwa.elasticsearch.authplugin;
+
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.rest.*;
+
+public class AuthRestHandler implements RestHandler {
+  private final Logger logger = Loggers.getLogger(AuthRestHandler.class);
+  private final RestHandler originalHandler;
+  private final RestResponse unauthorizedResponse = new RestResponse() {
+    @Override
+    public String contentType() {
+      return "application/json";
+    }
+
+    @Override
+    public BytesReference content() {
+      return new BytesArray("{\"error\": \"Unauthorized.\"}");
+    }
+
+    @Override
+    public RestStatus status() {
+      return RestStatus.UNAUTHORIZED;
+    }
+  };
+
+  AuthRestHandler(RestHandler restHandler) {
+    this.originalHandler = restHandler;
+  }
+
+  @Override
+  public void handleRequest(RestRequest restRequest, RestChannel restChannel, NodeClient nodeClient) throws Exception {
+    this.logger.info(restRequest.path());
+    this.logger.info(restRequest.rawPath());
+    if (isOk(restRequest)) {
+      this.originalHandler.handleRequest(restRequest, restChannel, nodeClient);
+    } else {
+      restChannel.sendResponse(unauthorizedResponse);
+    }
+  }
+
+  private boolean isOk(RestRequest restRequest) {
+    for (java.util.Map.Entry<String, String> entry: restRequest.headers()) {
+      String key = entry.getKey();
+      String value = entry.getValue();
+      if (key.equals("user") && value.equals("admin")) {
+        return true;
+      }
+    }
+    return false;
+
+    // ES 5.4
+    // return restRequest.getHeaders().get("user").equals("admin");
+  }
+}
